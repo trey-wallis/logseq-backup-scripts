0f931adbc39e2d2233c3a3dc872f284d0f724317
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-11-12 00:13:08 -0700

    Added: backup files only if they haven't changed and keep 3 copies

--- backup_remote.sh
+++ backup_remote.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: ./backup_sync.sh <source-folder> <user@remote> <ssh-key>" >&2
+if [ "$#" -ne 5 ]; then
+    echo "Usage: ./backup_sync.sh <source-folder> <user> <host> <ssh-key> <remote-folder>"
     exit 2
 fi
 
-source="$1"
-remote="$2"
-ssh_key="$3"
+source_folder="$1"
+user="$2"
+host="$3"
+ssh_key="$4"
+remote_folder="$5"
 
+# Validate that the source folder is a directory
+if [ -f "$source_folder" ]; then
+    echo "Error: Source folder cannot be a file." >&2
+    exit 1
+fi
+
+# MD5 hash all of the files and make a hash out of those hashes
+checksum="$(find -s "$source_folder" -type f -exec md5 {} \; | md5)"
+checksum_file="/tmp/$checksum"
+
+# If md5 hash hasn't changed, exit.
+if [ -f "$checksum_file" ]; then
+    echo "No file changes found."
+    exit 0
+fi
+
+# Create checksum file
+touch "$checksum_file"
+
+# Create file name based on timestamp
 current_timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-file_name="second_brain_$current_timestamp.tar.gz"
+file_name="vault_$current_timestamp.tar.gz"
 
-output="/tmp/$file_name"
+# Create a file from stdout. Then list all files, delete all but the last 3
+command="cat > $remote_folder/$file_name && cd $remote_folder && ls -t | tail -n +4 | xargs --no-run-if-empty rm --"
 
+# Create an gzipped achive of the source folder and send it over stdout
+# to our server
 # c - Create an archive
 # z - gzip
 # f - write to file
-# C - change the output path
-tar zcf - "$source" | ssh -i "$ssh_key" "$remote" "cat > ~/second_brain/$file_name"
+tar zcf - "$source_folder" | ssh -i "$ssh_key" "$user@$host" "$command"
 
-echo "$file_name"
+echo "$remote_folder/$file_name"
+exit 0
